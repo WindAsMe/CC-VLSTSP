1a419584831cf3c977c5ad3aaba8e5950f6b045d
ISDL <ISDL@ISDL-Public-iMac.local>
2022-09-21 12:15:37 +0900

    first commit

--- EAs/initial.py
+++ EAs/initial.py
@@ -1,14 +1,14 @@
 import copy
 import random
-
 import numpy as np
-from geatpy import scaling
+
+import helps
 
 
-def init_population(sub_cities, adj_matrix, NIND, K=50):
+def init_population(indexes, sub_cities, NIND, K=50):
     """
-    :param sub_cities: real sub_cities indexes
-    :param adj_matrix: distance between city i and city j
+    :param indexes: indexes of cities
+    :param sub_cities: coordinates of cities
     :param NIND: population size
     :param K: random chosen city
     :return: generated population
@@ -16,31 +16,30 @@ def init_population(sub_cities, adj_matrix, NIND, K=50):
 
     pop = []
     for i in range(NIND):
-        pop.append(K_greedy(sub_cities, adj_matrix, K))
+        pop.append(K_greedy(indexes, sub_cities, K))
     return np.array(pop)
 
 
-def K_greedy(sub_cities, adj_matrix, K):
-    temp_sub_cities = copy.deepcopy(sub_cities)
-    tour = [temp_sub_cities.pop(0)]
-    while len(temp_sub_cities) > 0:
-        K = min(K, len(temp_sub_cities))
+def K_greedy(indexes, sub_cities, K):
+    temp_indexes = copy.deepcopy(indexes)
+    tour = [temp_indexes.pop(0)]
+    while len(temp_indexes) > 0:
+        K = min(K, len(temp_indexes))
         current_city = tour[len(tour) - 1]
-        candidate_cities = random.sample(temp_sub_cities, K)
-        next = next_city(current_city, candidate_cities, adj_matrix)
-        tour.append(next)
-        temp_sub_cities.remove(next)
+        candidate_cities = random.sample(temp_indexes, K)
+        next = next_city(current_city, candidate_cities, sub_cities)
+        tour.append(temp_indexes.pop(next))
     return tour
 
 
-def next_city(current, candidates, adj_matrix):
-    next = candidates[0]
-    dis = adj_matrix[current][next]
+def next_city(current, candidates, sub_cities):
+    next = 0
+    dis = helps.Dis(sub_cities[current], sub_cities[next])
     for i in range(1, len(candidates)):
-        new_dis = adj_matrix[current][candidates[i]]
+        new_dis = helps.Dis(sub_cities[current], sub_cities[candidates[i]])
         if new_dis < dis:
             dis = new_dis
-            next = candidates[i]
+            next = i
     return next
 
 
--- main.py
+++ main.py
@@ -11,18 +11,18 @@ from EAs import initial
 if __name__ == "__main__":
     data_path = os.path.dirname(os.path.abspath(__file__)) + "/TSP/"
 
-    tsp_files = ['ics39603.tsp', 'rbz43748.tsp', 'fht47608.tsp', 'fna52057.tsp', 'bna56769.tsp', 'dan59296.tsp',
-                 'sra104815.tsp', 'ara238025.tsp', 'lra498378.tsp',  'lrb744710.tsp']
-    names = ['ics39603', 'rbz43748', 'fht47608', 'fna52057', 'bna56769', 'dan59296', 'sra104815', 'ara238025',
-             'lra498378', 'lrb744710']
-    scales = [39603, 43748, 47608, 52057, 56769, 59296, 104815, 238025, 498378, 744710]
-    # tsp_files = ['xqf131.tsp']
-    # names = ['xqf131']
-    # scales = [131]
+    # tsp_files = ['ics39603.tsp', 'rbz43748.tsp', 'fht47608.tsp', 'fna52057.tsp', 'bna56769.tsp', 'dan59296.tsp',
+    #              'sra104815.tsp', 'ara238025.tsp', 'lra498378.tsp',  'lrb744710.tsp']
+    # names = ['ics39603', 'rbz43748', 'fht47608', 'fna52057', 'bna56769', 'dan59296', 'sra104815', 'ara238025',
+    #          'lra498378', 'lrb744710']
+    # scales = [39603, 43748, 47608, 52057, 56769, 59296, 104815, 238025, 498378, 744710]
+    tsp_files = ['xqf131.tsp']
+    names = ['xqf131']
+    scales = [131]
     skip = 8
     trial_runs = 25
-    NIND = 100
-    Max_iter = 500
+    NIND = 10
+    Max_iter = 50
 
     for inst in range(1):
         Greedy_best_path = os.path.dirname(os.path.abspath(__file__)) + "/Data/Greedy/best_Dis/" + names[inst] + ".csv"
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
         for run in range(1):
             # GA.GA_exe(cities, NIND, Max_iter)
-            best_fitness, best_tour = hCCGA.hCCGA_exe(cities, NIND, Max_iter, adj_matrix, sub_size=10, K=10)
+            best_fitness, best_tour = hCCGA.hCCGA_exe(cities, NIND, Max_iter, adj_matrix, sub_size=20, K=10)
             print(helps.tour_Dis(best_tour, adj_matrix))
             print(best_fitness, best_tour)
 
